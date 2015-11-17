75237d04ff267e9ae6db54dde166e668f2dc4809
Guilherme Sanches Pereira <guilherme_1993pa@hotmail.com>
2015-11-17 21:47:59 +0000

    Second Exam alter name folder

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ var mysql = require('mysql');
 var path = require("path");
 
 
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
